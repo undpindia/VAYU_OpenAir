0e927d95373db1d3152a4c358fc314abe3494910
Maneesh7012 <maneesh@misteo.co>
2024-09-04 17:35:11 +0530

    download button style fix

--- vayu/web_portal/src/components/ui/dialog.jsx
+++ vayu/web_portal/src/components/ui/dialog.jsx
@@ -60,7 +60,7 @@ const DialogFooter = ({
   ...props
 }) => (
   <div
-    className={cn("flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2", className)}
+    className={cn("flex flex-row space-x-2 sm:justify-end sm:space-x-2", className)}
     {...props} />
 )
 DialogFooter.displayName = "DialogFooter"
