2a269db3e8ae71de0113d8fee428c666488dd7e9
hridayanp <hridayanphukan123@gmail.com>
2024-09-09 12:35:49 +0530

    extra

--- vayu/web_portal/src/pages/Sections/Feedback/Feedback.jsx
+++ vayu/web_portal/src/pages/Sections/Feedback/Feedback.jsx
@@ -1,7 +1,7 @@
 const Feedback = () => {
   return (
-    <div className="w-full flex flex-col items-start p-4 space-y-6">
-      <span className="font-normal text-[20px] sm:p-0 md:text-[14px] md:leading-[20px]  lg:text-[24px] lg:leading-[28.16px] leading-[28.16px] md:p-2 lg:p-[20px] lg:text-left md:text-left sm:text-left">
+    <div className="w-full flex flex-col items-start p-0 space-y-6">
+      <span className="font-normal text-[20px] sm:p-0 md:text-[14px] md:leading-[20px]  lg:text-[24px] lg:leading-[28.16px] leading-[28.16px] md:p-2 lg:p-[20px] text-justify">
         If you have any questions or comments about Vayu, please leave a comment
         on the Discussions tab of our <span></span>
         <a
